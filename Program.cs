9e0ebec97be02a2ccffc09861d99b23368a6237d
yosuna@xyncro.net <adiel2008@gmail.com>
2023-09-28 16:27:53 -0400

    refactor

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using core;
-public interface Interface1
+﻿public interface Interface1
 {
     public int Method1();
     public int Method2();
@@ -36,7 +29,7 @@ namespace CSharpTutorials
         static void Main(string[] args)
         {
             A a = new A();
-            var proxy = ProxyErrorHandler<Interface1>.CreateProxy(a);
+            var proxy = core.ProxyErrorHandler<Interface1>.CreateProxy(a);
             Interface1 p = proxy as Interface1;
             Console.WriteLine(p.Method1());
             Console.WriteLine(p.Method2());
