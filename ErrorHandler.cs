36c83d85f22941e03c77901b9fe9748fb91bed80
yosuna@xyncro.net <adiel2008@gmail.com>
2023-09-28 16:23:00 -0400

    initial

---
+++ ErrorHandler.cs
@@ -0,0 +1,60 @@
+using System.Formats.Tar;
+using System.Reflection;
+using System;
+
+namespace core
+{
+
+    public static class TypeExtensions
+    {
+        public static object? GetDefaultValue(this Type t)
+        {
+            if (t.IsValueType && Nullable.GetUnderlyingType(t) == null)
+                return Activator.CreateInstance(t);
+            else
+                return null;
+        }
+    }
+    public class ProxyErrorHandler<I> : DispatchProxy where I : class
+    {
+        public I Target { get; set; }
+
+        public Action<MethodInfo , object[]> PreInvokeHandler = (MethodInfo targetMethod, object[] args) =>{
+            Console.WriteLine($"Called {targetMethod.Name}");
+        };
+
+        public Func<System.Exception, bool> ExceptionLogger = (System.Exception ex) =>{
+            Console.WriteLine(ex.Message);
+            return false;
+        };
+
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            PreInvokeHandler?.Invoke(targetMethod, args);
+            
+            try{
+                var result = targetMethod.Invoke(Target, args);
+                return result;
+            }
+            catch(ApplicationException ex)
+            {
+                if(ExceptionLogger?.Invoke(ex.InnerException) ?? true)
+                    throw ex;
+            }
+            catch(Exception ex)
+            {
+                if(ExceptionLogger?.Invoke(ex) ?? true)
+                    throw ex;
+            }
+
+            return targetMethod.ReturnType.GetDefaultValue();
+        }
+
+        public static ProxyErrorHandler<I> CreateProxy<T>(T target)  where T : class, I
+        {
+            var proxy = Create<I, ProxyErrorHandler<I>>() as ProxyErrorHandler<I>;
+            proxy.Target = target;
+            return proxy;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using core;
+public interface Interface1
+{
+    public int Method1();
+    public int Method2();
+
+    public void Method3();
+    public void Method4();
+}
+class A : Interface1{
+
+    public int Method1()
+    {
+        return 1;
+    }
+
+    public int Method2()
+    {
+        throw new Exception("My exception");
+    }
+
+    public void Method3(){}
+    public void Method4(){}
+}
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            var proxy = ProxyErrorHandler<Interface1>.CreateProxy(a);
+            Interface1 p = proxy as Interface1;
+            Console.WriteLine(p.Method1());
+            Console.WriteLine(p.Method2());
+
+            p.Method3();
+            p.Method4();
+
+            proxy.ExceptionLogger = (System.Exception ex) =>{
+                Console.WriteLine(ex.Message);
+                return true; // rethrow the exception
+            };
+
+            try{
+                Console.WriteLine(p.Method2());
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Exception rethrown");
+            }
+
+        }
+    }
+}
