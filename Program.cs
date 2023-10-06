4885ba6c58d1cfe9311ca52486760ba62f120782
yosuna@xyncro.net <adiel2008@gmail.com>
2023-10-05 23:01:10 -0400

    w

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿public interface Interface1
+﻿using core;
+using System.Xml.Linq;
+
+public interface Interface1
 {
     public int Method1();
     public int Method2();
@@ -21,6 +24,10 @@ class A : Interface1{
     public void Method3(){}
     public void Method4(){}
 }
+class M<T>
+{
+
+}
 
 namespace CSharpTutorials
 {
@@ -28,10 +35,37 @@ namespace CSharpTutorials
     {
         static void Main(string[] args)
         {
+            //var mm = new TResult<int>(2);
+            //int gg = mm;
+
             A a = new A();
+
+            //Type theType = Type.GetType("core.TResult`1");
+            var fffff = typeof(TResult<>);
+
+            
+
+            object v1 = 9;
+
+
+            Type typeArgument = v1.GetType();
+            Type template = typeof(core.TResult<>);
+            Type t4 = Type.GetType(fffff.FullName);
+            Type genericType = template.MakeGenericType(typeArgument);
+            var instance = Activator.CreateInstance(genericType, 2);
+
+            int tyrttry = (core.TResult<int>)instance;
+
+            //int hhh = fffff.
+
+            int mm = (int)instance;
+
+
             var proxy = core.ProxyErrorHandler<Interface1>.CreateProxy(a);
             Interface1 p = proxy as Interface1;
-            Console.WriteLine(p.Method1());
+            var f = p.Method1();
+            int h = f;
+            Console.WriteLine(f);
             Console.WriteLine(p.Method2());
 
             p.Method3();
